d397fc4c4e6327035fada4a69314958beb86a9f2
tuukka <tuukka.pohjonen@outlook.com>
2021-03-24 20:17:39 +0200

    ADD login and registering to program

--- app/src/main/java/com/example/bookkeeping/dbinterface.java
+++ app/src/main/java/com/example/bookkeeping/dbinterface.java
@@ -3,10 +3,17 @@ package com.example.bookkeeping;
 import androidx.room.Dao;
 import androidx.room.Insert;
 import androidx.room.OnConflictStrategy;
+import androidx.room.Query;
+
+import java.util.List;
 
 @Dao
 public interface dbinterface {
     @Insert
     void registerUser(userdetails userdetails);
+
+    @Query("SELECT users.userid, users.password from users where userName=(:username) and password=(:password)")
+    userdetails login(String username, String password);
+
 
 }
--- app/src/main/java/com/example/bookkeeping/login.java
+++ app/src/main/java/com/example/bookkeeping/login.java
@@ -11,6 +11,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class login extends AppCompatActivity {
     Button loginButton;
@@ -22,8 +23,8 @@ public class login extends AppCompatActivity {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.s1_login);
-        username = (EditText)findViewById(R.id.username_);
-        password = (EditText)findViewById(R.id.password_);
+        username = (EditText) findViewById(R.id.username_);
+        password = (EditText) findViewById(R.id.password_);
         loginButton = findViewById(R.id.loginButton);
 
     }
@@ -35,12 +36,42 @@ public class login extends AppCompatActivity {
         String tempUsername = "qwerty";
         String tempPassword = "asdf";
 
+        final String usernametocheck = username.getText().toString();
+        final String passwordtocheck = password.getText().toString();
+
 
         System.out.println(password.getText().toString());
-         if ((username.getText().toString().equals(tempUsername)) & (password.getText().toString().equals(tempPassword))) {
-            System.out.println("Sisään");
-             Intent intent = new Intent(this, register.class);
-             startActivity(intent);
+        if (usernametocheck.isEmpty() || passwordtocheck.isEmpty()) {
+            Toast.makeText(getApplicationContext(), "Enter username and password", Toast.LENGTH_SHORT).show();
+
+        } else {
+            userdatabase userdatabase = com.example.bookkeeping.userdatabase.getuserDatabase(getApplicationContext());
+            final dbinterface dbinterface = userdatabase.dbinterface();
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+
+
+                    userdetails userdetails = dbinterface.login(usernametocheck, passwordtocheck);
+                    if (userdetails == null) {
+
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                Toast.makeText(getApplicationContext(), "Invalid credentials.", Toast.LENGTH_SHORT).show();
+
+                            }
+                        });
+                    } else {
+
+                        //Intent intent = new Intent(this, menu.class);
+                        startActivity(new Intent(login.this, menu.class));
+
+                    }
+                }
+
+            }).start();
+
         }
 
     }
